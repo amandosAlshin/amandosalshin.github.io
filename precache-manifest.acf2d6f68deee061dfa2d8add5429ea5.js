217563a47cccc6fe4616dce765162b2169fa3fa4
amandosAlshin <amandos94@gmail.com>
2020-09-24 10:59:52 +0600

    first commit

---
+++ precache-manifest.acf2d6f68deee061dfa2d8add5429ea5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50325f4f17d157de15258ad659732eae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54334df4cf78d14bc49a",
+    "url": "/static/css/main.f47c9a98.chunk.css"
+  },
+  {
+    "revision": "b30d14c287d69c42e970",
+    "url": "/static/js/2.4983afac.chunk.js"
+  },
+  {
+    "revision": "6ead315f60788879c491a87054985612",
+    "url": "/static/js/2.4983afac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54334df4cf78d14bc49a",
+    "url": "/static/js/main.640ce92b.chunk.js"
+  },
+  {
+    "revision": "750653fc2386e052083d",
+    "url": "/static/js/runtime-main.bf04950f.js"
+  }
+]);
